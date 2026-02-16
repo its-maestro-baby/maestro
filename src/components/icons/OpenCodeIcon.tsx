59c4c8928d3c8abe32fd6f23756fd6ec5946a992
Thomas Wheeler <t_wheeler2002@yahoo.com>
2026-02-15 18:39:36 -1000

    Use correct SVG data for OpenCode icon

--- src/components/icons/OpenCodeIcon.tsx
+++ src/components/icons/OpenCodeIcon.tsx
@@ -1,7 +1,7 @@
 import type { SVGProps } from "react";
 
 interface OpenCodeIconProps extends SVGProps<SVGSVGElement> {
-  size?: number | string;
+	size?: number | string;
 }
 
 /**
@@ -10,22 +10,43 @@ interface OpenCodeIconProps extends SVGProps<SVGSVGElement> {
  * Accepts same props as Lucide icons for consistency.
  */
 export function OpenCodeIcon({ size = 24, className = "", ...props }: OpenCodeIconProps) {
-  return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      aria-label="OpenCode"
-      {...props}
-    >
-      <title>OpenCode</title>
-      <path
-        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1.5 15.5h-2v-7h2v7zm5 0h-2v-7h2v7zm-2.5-9.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"
-        fill="currentColor"
-      />
-    </svg>
-  );
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 240 300"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			className={className}
+			aria-label="OpenCode"
+			{...props}
+		>
+			<title>OpenCode</title>
+			<g clipPath="url(#clip0_1401_86274)">
+				<mask
+					id="mask0_1401_86274"
+					style={{ maskType: "luminance" }}
+					maskUnits="userSpaceOnUse"
+					x="0"
+					y="0"
+					width="240"
+					height="300"
+				>
+					<path d="M240 0H0V300H240V0Z" fill="white" />
+				</mask>
+				<g mask="url(#mask0_1401_86274)">
+					<path d="M180 240H60V120H180V240Z" fill="currentColor" />
+					<path
+						d="M180 60H60V240H180V60ZM240 300H0V0H240V300Z"
+						fill="currentColor"
+					/>
+				</g>
+			</g>
+			<defs>
+				<clipPath id="clip0_1401_86274">
+					<rect width="240" height="300" fill="white" />
+				</clipPath>
+			</defs>
+		</svg>
+	);
 }
